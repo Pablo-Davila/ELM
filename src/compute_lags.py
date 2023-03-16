1e9b0b9ecdb09f5390ba8267938cbb539496656b
Pablo-Davila <Pablo-Davila@users.noreply.github.com>
2023-03-16 17:40:06 +0000

    Change predicted attribute to VAR 01

--- src/compute_lags.py
+++ src/compute_lags.py
@@ -8,43 +8,41 @@ DATASET_PLACE = "Varennes"
 DATASET_FREC = "1000"
 PH = 180
 FH = 30
-TEST_SIZE = 0.2
+TEST_SIZE = 0.2  # TEMP
 
-DATASET_PATH = f"./data/{DATASET_DAY}/{DATASET_PLACE}_{DATASET_FREC}ms.csv"
+BASE_PATH = f"./data/{DATASET_DAY}/{DATASET_PLACE}_{DATASET_FREC}ms"
 
 
-data = pd.read_csv(DATASET_PATH)
+data = pd.read_csv(f"{BASE_PATH}.csv")
 data.drop("Unnamed: 0", axis=1, inplace=True)  # remove date column
 
 train_size = int(len(data)*(1-TEST_SIZE))
 
-# TODO Averiguar qué sensor estuvo prediciendo Pedro
-# Estoy utilizando el último. No debería ser muy determinante.
-x_train = data.values[:train_size-FH, :-1]
+x_train = data.values[:train_size-FH, 1:]
 x_train = np.hstack([
     x_train[i:len(x_train)-PH+i, :]
     for i in range(PH)
 ])
-np.save(f"./data/{DATASET_DAY}/{DATASET_PLACE}_{DATASET_FREC}ms_{PH}ph_{FH}fh_x_train_{TEST_SIZE}.npy", x_train)
+np.save(f"{BASE_PATH}_{PH}ph_{FH}fh_x_train_{TEST_SIZE}.npy", x_train)
 
-y_train = data.values[PH:train_size, -1]
+y_train = data.values[PH:train_size, 0]
 y_train = np.hstack([
     y_train[i:len(y_train)-FH+i].reshape(-1, 1)
     for i in range(FH)
 ])
-np.save(f"./data/{DATASET_DAY}/{DATASET_PLACE}_{DATASET_FREC}ms_{PH}ph_{FH}fh_y_train_{TEST_SIZE}.npy", y_train)
+np.save(f"{BASE_PATH}_{PH}ph_{FH}fh_y_train_{TEST_SIZE}.npy", y_train)
 
-x_test = data.values[train_size:-FH, :-1]
+x_test = data.values[train_size:-FH, 1:]
 x_test = np.hstack([
     x_test[i:len(x_test)-PH+i, :]
     for i in range(PH)
 ])
-np.save(f"./data/{DATASET_DAY}/{DATASET_PLACE}_{DATASET_FREC}ms_{PH}ph_{FH}fh_x_test_{TEST_SIZE}t.npy", x_test)
+np.save(f"{BASE_PATH}_{PH}ph_{FH}fh_x_test_{TEST_SIZE}t.npy", x_test)
 
-y_test = data.values[train_size+PH:, -1]
+y_test = data.values[train_size+PH:, 0]
 y_test = np.hstack([
     y_test[i:len(y_test)-FH+i].reshape(-1, 1)
     for i in range(FH)
 ])
-np.save(f"./data/{DATASET_DAY}/{DATASET_PLACE}_{DATASET_FREC}ms_{PH}ph_{FH}fh_y_test_{TEST_SIZE}t.npy", y_test)
+np.save(f"{BASE_PATH}_{PH}ph_{FH}fh_y_test_{TEST_SIZE}t.npy", y_test)
 
