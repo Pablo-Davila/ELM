5618dbd0aafe384154e0b5aa821784eac1269461
Pablo-Davila <Pablo-Davila@users.noreply.github.com>
2023-05-14 16:59:55 +0100

    Improve formatting. Rename activation and loss functions.

--- src/numpy_oselm.py
+++ src/numpy_oselm.py
@@ -1,9 +1,10 @@
+
 from math import ceil
 
 import numpy as np
 
 
-def activation(x):
+def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
@@ -13,6 +14,12 @@ def mean_squared_error(a, b):
 
 class OS_ELM(object):
     def __init__(self, n_input_nodes, n_hidden_nodes, n_output_nodes):
+        # TEMP Implement more activation functions
+        self.activation = sigmoid
+
+        # TEMP Implement more loss functions
+        self.loss = mean_squared_error
+
         self.__n_input_nodes = n_input_nodes
         self.__n_hidden_nodes = n_hidden_nodes
         self.__n_output_nodes = n_output_nodes
@@ -20,15 +27,26 @@ class OS_ELM(object):
         self.__is_finished_init_train = False
 
         # alpha = learning rate
-        self.__alpha = np.array([[np.random.uniform(-1, 1) for _ in range(self.__n_hidden_nodes)] for i in range(self.__n_input_nodes)])
-        self.__bias = np.array([np.random.uniform(-1, 1) for _ in range(self.__n_hidden_nodes)])
+        self.__alpha = np.array([
+            [np.random.uniform(-1, 1) for _ in range(self.__n_hidden_nodes)]
+            for i in range(self.__n_input_nodes)
+        ])
+        self.__bias = np.array([
+            np.random.uniform(-1, 1)
+            for _ in range(self.__n_hidden_nodes)
+        ])
         # beta is the weight of precision over recall in F-beta score
-        self.__beta = np.zeros(shape=[self.__n_hidden_nodes, self.__n_output_nodes])
+        self.__beta = np.zeros(
+            shape=[self.__n_hidden_nodes, self.__n_output_nodes])
         # model parameter
-        self.__p = np.zeros(shape=[self.__n_hidden_nodes, self.__n_hidden_nodes])
+        self.__p = np.zeros(
+            shape=[self.__n_hidden_nodes, self.__n_hidden_nodes])
 
     def predict(self, x):
-        return np.dot(activation(np.dot(x, self.__alpha) + self.__bias), self.__beta)
+        return np.dot(
+            self.activation(np.dot(x, self.__alpha) + self.__bias),
+            self.__beta
+        )
 
     def evaluate(self, x, t, metrics=None):
         if metrics is None:
@@ -36,7 +54,7 @@ class OS_ELM(object):
         met = []
         for m in metrics:
             if m == 'loss':
-                met.append(mean_squared_error(self.predict(x), t))
+                met.append(self.loss(self.predict(x), t))
             elif m == 'accuracy':
                 tp = fp = 0
                 for i in range(len(x)):
@@ -46,31 +64,37 @@ class OS_ELM(object):
                         fp += 1
                 met.append((tp / (tp + fp)))
             else:
-                return ValueError('An unknown metric \'{}\' was given.'.format(m))
+                return ValueError(f"An unknown metric '{m}' was given.")
 
         return met
 
     def init_train(self, x, t):
         if self.__is_finished_init_train:
-            raise Exception('The initial training phase has already finished. Please call \'seq_train\' method for '
-                            'further training.')
+            raise Exception(
+                "The initial training phase has already finished. Please call "
+                "'seq_train' method for further training.")
         if len(x) < self.__n_hidden_nodes:
-            raise ValueError('In the initial training phase, the number of training samples must be greater than the '
-                             'number of hidden nodes. But this time len(x)={}, while n_hidden_nodes={}'
-                             .format(len(x), self.__n_hidden_nodes))
+            raise ValueError(
+                "In the initial training phase, the number of training samples must be "
+                "greater than the number of hidden nodes. But this time "
+                f"len(x)={len(x)}, while n_hidden_nodes={self.__n_hidden_nodes}"
+            )
 
         self.__build_init_train_graph(x, t)
         self.__is_finished_init_train = True
 
     def seq_train(self, x, t):
         if not self.__is_finished_init_train:
-            raise Exception('You have not gone through the initial training phase yet. Please first initialize the '
-                            'model\'s weights by \'init_train\' method before calling \'seq_train\' method.')
+            raise Exception(
+                "You have not gone through the initial training phase yet. Please "
+                "first initialize the model's weights by 'init_train' method before "
+                "calling 'seq_train' method."
+            )
         self.__build_seq_train_graph(x, t)
 
     def __build_init_train_graph(self, x, t):
         # hidden layer output matrix
-        H = activation(np.dot(x, self.__alpha) + self.__bias)
+        H = self.activation(np.dot(x, self.__alpha) + self.__bias)
         HT = np.transpose(H)
         HTH = np.dot(HT, H)
         self.__p = np.linalg.inv(HTH)
@@ -80,16 +104,16 @@ class OS_ELM(object):
 
     def __build_seq_train_graph(self, x, t):
         # hidden layer output matrix
-        H = activation(np.dot(x, self.__alpha) + self.__bias)
-        HT = np.transpose(H)
+        h = self.activation(np.dot(x, self.__alpha) + self.__bias)
+        ht = np.transpose(h)
         batch_size = x.shape[0]
-        I = np.eye(batch_size)
-        Hp = np.dot(H, self.__p)
-        HpHT = np.dot(Hp, HT)
-        temp = np.linalg.inv(I + HpHT)
-        pHT = np.dot(self.__p, HT)
-        self.__p -= np.dot(np.dot(pHT, temp), Hp)
-        pHT = np.dot(self.__p, HT)
-        Hbeta = np.dot(H, self.__beta)
-        self.__beta += np.dot(pHT, t - Hbeta)
+        i = np.eye(batch_size)
+        hp = np.dot(h, self.__p)
+        hp_ht = np.dot(hp, ht)
+        temp = np.linalg.inv(i + hp_ht)
+        pht = np.dot(self.__p, ht)
+        self.__p -= np.dot(np.dot(pht, temp), hp)
+        pht = np.dot(self.__p, ht)
+        hbeta = np.dot(h, self.__beta)
+        self.__beta += np.dot(pht, t - hbeta)
         return self.__beta
