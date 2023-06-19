97c00a384ea9a5f07abe7eca34affc394e0bb467
Pablo-Davila <Pablo-Davila@users.noreply.github.com>
2023-06-19 10:56:32 +0200

    Add htree model run

---
+++ src/htree_run.py
@@ -0,0 +1,166 @@
+
+import json
+import os
+import time
+
+import numpy as np
+from river import tree
+from river import multioutput
+from river import stream
+import tqdm
+
+from eval import mae, mape
+
+
+DATASET_DAY = "Variable"
+# DATASET_DAY = "Very variable"
+DATASET_PLACE = "Varennes"
+DATASET_FREC = "1000"  # For "Variable" (1 ins/s)
+# DATASET_FREC = "500"  # For "Very variable" (2 ins/s)
+TEST_SIZE = 0.0
+PH = 100  # For "Variable"
+# PH = 180  # For "Variable"
+# PH = 200  # For "Very variable"
+# PH = 360  # For "Very variable"
+FH = 10  # For "Variable"
+# FH = 30  # For "Variable"
+# FH = 20  # For "Very variable"
+# FH = 60  # For "Very variable"
+MODEL_CODE = "htree"
+INITIAL_TRAIN_SIZE = 2
+MAX_DEPTH = [6, 10, 14]
+ALPHA = 0.99
+
+DATE = time.strftime("%Y-%m-%d_%H-%M", time.localtime())
+BASE_PATH = f"./data/{DATASET_DAY}/{DATASET_PLACE}_{DATASET_FREC}ms"
+OUT_PATH = f"./results/{DATASET_DAY}/{DATE}_{MODEL_CODE}_ph{PH}_fh{FH}.json"
+
+
+# Read datasets
+with open(f"{BASE_PATH}_params.json") as fparams:
+    params = json.load(fparams)
+max_value = params["max"]
+
+x_data = np.load(f"{BASE_PATH}_{PH}ph_{FH}fh_x_train_{TEST_SIZE}.npy")
+y_data = np.load(f"{BASE_PATH}_{PH}ph_{FH}fh_y_train_{TEST_SIZE}.npy")
+
+# Perform tests
+count = 0
+results = []
+for max_depth in MAX_DEPTH:
+    print(count)
+
+    # Set model parameters
+    optimizer_log = "optim.SGD(0.01)"
+    loss_log = "optim.losses.Squared()"
+    params = dict(
+        grace_period=200,
+        max_depth=max_depth,
+        delta=1e-7,  # TEMP
+        tau=0.05,  # TEMP
+        leaf_prediction="adaptive",  # TEMP
+        leaf_model=None,  # linear regression
+        model_selector_decay=0.95,  # TEMP
+        nominal_attributes=None,
+        splitter=None,
+        min_samples_split=5,  # TEMP
+        binary_split=False,
+        max_size=500.0,  # TEMP
+        memory_estimate_period=1_000_000,
+        stop_mem_management=False,  # TEMP
+        remove_poor_attrs=False,
+        merit_preprune=True,  # TEMP
+    )
+
+    # Build model
+    model = multioutput.RegressorChain(
+        tree.HoeffdingTreeRegressor(**params)
+    )
+
+    # Split initial and sequential data
+    assert INITIAL_TRAIN_SIZE <= x_data.shape[0]
+    x_initial = x_data[:INITIAL_TRAIN_SIZE, :]
+    y_initial = y_data[:INITIAL_TRAIN_SIZE, :]
+    data_initial = stream.iter_array(x_initial, y_initial)
+    x_sequential = x_data[INITIAL_TRAIN_SIZE:, :]
+    y_sequential = y_data[INITIAL_TRAIN_SIZE:, :]
+    data_sequential = stream.iter_array(x_sequential, y_sequential)
+
+    t0 = time.time()  # Timer start
+
+    # Initial train
+    pbar = tqdm.tqdm(
+        total=x_data.shape[0],
+        desc="Initial training phase"
+    )
+    for x, y in data_initial:
+        model.learn_one(x, y)
+        pbar.update(n=1)
+
+    # Sequential train-test
+    pbar.set_description("Sequential training phase")
+    si_mae = 0
+    si_mape = 0
+    bi = 0
+    pmae = []
+    pmape = []
+
+    for x, y in data_sequential:
+
+        # Evaluate model
+        yi = {k: max_value*e for k, e in y.items()}
+        oi = {k: max_value*e for k, e in model.predict_one(x).items()}
+
+        si_mae = mae(yi, oi) + ALPHA*si_mae
+        si_mape = mape(yi, oi) + ALPHA*si_mape
+        bi = 1 + ALPHA * bi
+        pmae.append(si_mae / bi)
+        pmape.append(si_mape / bi)
+
+        # Train model
+        model.learn_one(x, y)
+
+        pbar.update(n=1)
+
+    duration = time.time() - t0  # Timer end
+    pbar.close()
+
+    pmae_mean = np.array(pmae).mean()
+    pmae_std = np.array(pmae).std()
+    pmape_mean = np.array(pmape).mean()
+    pmape_std = np.array(pmape).std()
+
+    # Output results
+    print(
+        f"{pmae_mean=:.4f}",
+        f"{pmae_std=:.4f}",
+        f"{pmape_mean=:.4f}",
+        f"{pmape_std=:.4f}",
+        f"{duration=:.4f}",
+    )
+
+    params["optimizer"] = optimizer_log
+    params["loss"] = loss_log
+    result = dict(
+        pmae_mean=pmae_mean,
+        pmae_std=pmae_std,
+        pmape_mean=pmape_mean,
+        pmape_std=pmape_std,
+        duration=duration,
+        params=params,
+    )
+    results.append(result)
+
+    count += 1
+
+    # Write results
+    if not os.path.exists(os.path.dirname(OUT_PATH)):
+        os.makedirs(os.path.dirname(OUT_PATH))
+    with open(f"{OUT_PATH}_porsi={count}", "w") as f:
+        json.dump(result, f)
+
+# Write results
+if not os.path.exists(os.path.dirname(OUT_PATH)):
+    os.makedirs(os.path.dirname(OUT_PATH))
+with open(OUT_PATH, "w") as f:
+    json.dump(results, f)
